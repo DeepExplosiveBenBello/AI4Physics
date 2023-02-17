889eaffaaf510decb1d925a726b2ab6f3b1e9d86
Oscar <49638680+oscar-defelice@users.noreply.github.com>
2023-02-17 22:21:10 +0100

    data, first functions, test added (#4)
    * data, first functions, test added
    
    * data, first functions, test added
    
    * chore: Modify directory structures
    
    * fix: Handle different location of program running
    
    * chore: Update pandas version
    
    * chore: Add gitignore
    
    * add docstrings, sanitychecks, structure the utility folder
    
    * Chore: test/utilities folder organized
    
    ---------
    
    Co-authored-by: garghi2000 <garghi2000@hotmail.it>

---
+++ tests/test_current_jumps.py
@@ -0,0 +1,17 @@
+import numpy as np
+from src.current_jumps import std_1D
+from src.current_jumps import get_n_moms_of_moving_array
+from tests.utilities.test_utilities import list_to_test_std_1D
+from tests.utilities.test_utilities import list_to_test_get_n_moms_of_moving_array
+import pytest
+
+@pytest.mark.parametrize("inputs, expected_result", list_to_test_std_1D())
+def test_std_1D(inputs, expected_result):
+    result = std_1D(*inputs)
+    assert result == expected_result
+
+@pytest.mark.parametrize("inputs, expected_result",
+                         list_to_test_get_n_moms_of_moving_array())
+def test_get_n_moms_of_moving_array(inputs, expected_result):
+    result = get_n_moms_of_moving_array(*inputs)
+    assert np.array_equal(result, expected_result)
